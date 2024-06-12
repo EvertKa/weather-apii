b26624e84f4eb34eb4924a47eec0c7b17267e1df
EvertKa <evert.karp@ametikool.ee>
2024-06-12 23:18:55 +0300

    final

--- deploy.php
+++ deploy.php
@@ -1,19 +1,33 @@
 <?php
-
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'contrib/php-fpm.php';
-require 'contrib/npm.php';
 
-set('application', 'mylaravelapp');
-set('repository', 'git@github.com:lorisleiva/mylaravelapp.git');
-set('php_fpm_version', '8.0');
+// Zone ühendus
+set('application', 'yl1');
+set('remote_user', 'virt109710');
+set('http_user', 'virt109710');
+set('keep_releases', 2);
 
-host('prod')
-    ->set('remote_user', 'root')
-    ->set('hostname', 'mylaravelapp.com')
-    ->set('deploy_path', '/var/www/{{hostname}}');
+host('tak21karp.itmajakas.ee')
+    ->setHostname('tak21karp.itmajakas.ee')
+    ->set('http_user', 'virt109710')
+    ->set('deploy_path', '~/domeenid/www.tak21karp.itmajakas.ee/yl1')
+    ->set('branch', 'main');
+
+set('repository', 'git@github.com:EvertKa/weather-apii.git');
+
+// tasks
+task('opcache:clear', function () {
+    run('killall php82-cgi || true');
+})->desc('Clear opcache');
+
+task('build:node', function () {
+    cd('{{release_path}}');
+    run('npm i');
+    run('npx vite build');
+    run('rm -rf node_modules'); 
+});
 
 task('deploy', [
     'deploy:prepare',
@@ -21,16 +35,46 @@ task('deploy', [
     'artisan:storage:link',
     'artisan:view:cache',
     'artisan:config:cache',
-    'artisan:migrate',
-    'npm:install',
-    'npm:run:prod',
+    'build:node',
     'deploy:publish',
-    'php-fpm:reload',
+    'opcache:clear',
+    'artisan:cache:clear'
 ]);
-
-task('npm:run:prod', function () {
-    cd('{{release_or_current_path}}');
-    run('npm run prod');
-});
+// Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+// namespace Deployer;
+
+// require 'recipe/laravel.php';
+// require 'contrib/php-fpm.php';
+// require 'contrib/npm.php';
+
+// set('application', 'yl1');
+// set('repository', 'git@github.com:EvertKa/weather-apii.git');
+// set('php_fpm_version', '8.0');
+
+// host('tak21karp.itmajakas.ee')
+//     ->set('remote_user', 'virt109710')
+//     ->set('hostname', 'tak21karp.itmajakas.ee')
+//     ->set('deploy_path', '~/domeenid/tak21karp.itmajakas.ee/yl1');
+
+// task('deploy', [
+//     'deploy:prepare',
+//     'deploy:vendors',
+//     'artisan:storage:link',
+//     'artisan:view:cache',
+//     'artisan:config:cache',
+//     'artisan:migrate',
+//     'npm:install',
+//     'npm:run:prod',
+//     'deploy:publish',
+//     'php-fpm:reload',
+// ]);
+
+// task('npm:run:prod', function () {
+//     cd('{{release_or_current_path}}');
+//     run('npm run prod');
+// });
+
+// after('deploy:failed', 'deploy:unlock');
